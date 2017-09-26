d11f236f19dff942ac571144f8016b3af4c44966
Kacper Łukawski <kacper.lukawski@codete.co>
2017-09-26 09:39:06 +0200

    Add packages to setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
 setup(
     name='sentiment-analysis-rd',
     version='1.0.0',
-    packages=['soundscrape'],
+    packages=['loader', 'message', 'preprocessing', 'vectorizer'],
     install_requires=required,
     extras_require={},
     include_package_data=True,
