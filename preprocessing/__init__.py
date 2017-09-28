49b141dd5c55dc94ec0b11f654e7c149330a646e
Kacper Łukawski <kacper.lukawski@codete.co>
2017-09-28 20:07:44 +0200

    Removed stopwords usage

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -14,7 +14,7 @@ class TextPreprocessor(object):
 
 class TwitterTextPreprocessor(TextPreprocessor):
 
-    ENGLISH_STOPWORDS = stopwords.words('english')
+    ENGLISH_STOPWORDS = stopwords.words("english")
     HASHTAG_REGEX = re.compile("#([a-z0-9_]+)", re.UNICODE)
     MENTION_REGEX = re.compile("@([a-z0-9_]+)", re.UNICODE)
     URL_REGEX = re.compile(r"(?:(http://)|(www\.))(\S+\b/?)"
@@ -26,7 +26,7 @@ class TwitterTextPreprocessor(TextPreprocessor):
         message = self.HASHTAG_REGEX.sub("\\1", message)
         message = self.MENTION_REGEX.sub("twitter_account", message)
         message = self.URL_REGEX.sub("external_link ", message)
-        message = " ".join(word for word in message.split()
-                           if word not in self.ENGLISH_STOPWORDS)
+        # message = " ".join(word for word in message.split()
+        #                    if word not in self.ENGLISH_STOPWORDS)
         logger.debug("Message '%s' preprocessed to '%s'", raw_message, message)
         return message
