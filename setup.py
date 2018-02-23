767c420549bff92d3bb240d9f240aad831431562
kacper <kacper.lukawski@codete.com>
2018-02-23 13:17:56 +0100

    Change package structure. Increase version to 1.1.4

--- setup.py
+++ setup.py
@@ -8,9 +8,12 @@ with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
 
 setup(
     name='sentiment-analysis-rd',
-    version='1.1.3',
-    package_dir={'sentiment': '.'},
-    packages=['sentiment'],
+    version='1.1.4',
+    package_dir={'sentiment': '.', 'sentiment.data': './data', 'sentiment.loader': './loader',
+                 'sentiment.message': './message', 'sentiment.preprocessing': './preprocessing',
+                 'sentiment.vectorizer': './vectorizer'},
+    packages=['sentiment', 'sentiment.data', 'sentiment.loader', 'sentiment.message', 'sentiment.preprocessing',
+              'sentiment.vectorizer'],
     install_requires=required,
     extras_require={},
     include_package_data=True,
